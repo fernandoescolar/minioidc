32dbf00a6c097f09cefd54503f24c589a54065a5
Fernando Escolar <fer.escolar@gmail.com>
2023-10-05 14:26:10 +0200

    better csp filter

--- internal/api/middlewares/csp.go
+++ internal/api/middlewares/csp.go
@@ -17,6 +17,6 @@ func NewCSP(config *domain.Config) *CSP {
 }
 
 func (m *CSP) ServeHTTP(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
-	w.Header().Set("Content-Security-Policy", "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self'; connect-src 'self'; media-src 'self'; object-src 'none'; frame-src 'none';")
+	w.Header().Set("Content-Security-Policy", "default-src 'self'; script-src 'self'; style-src 'self'; img-src 'self'; font-src 'self'; connect-src 'self'; media-src 'self'; object-src 'none'; frame-src 'none';frame-ancestors 'none';base-uri 'self';form-action 'self'")
 	next(w, r)
 }
